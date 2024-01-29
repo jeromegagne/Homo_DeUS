d7bed411140edd03deb41d335f01051a4c5882b7
tiblond <bera1107@usherbrooke.ca>
2024-01-28 21:53:21 -0500

    refit for linux and some cleaning

--- Modules/Dialogue/audioPipe.py
+++ Modules/Dialogue/audioPipe.py
@@ -7,13 +7,14 @@ from gtts import gTTS
 import sounddevice as sd
 from scipy.io.wavfile import write
 
+
 #NLP
 import nltk
 #from nltk.tokenize import word_tokenize
 #from nltk.corpus import stopwords
 #from nltk.stem import WordNetLemmatizer
 
-model = whisper.load_model("base")
+model = whisper.load_model("tiny")
 
 def record_audio(fs=44100, seconds=5, audioFilename='output.wav'):
   print("Enregistrement...")
@@ -30,32 +31,33 @@ def transcribe_audio(model, filename="output.wav"):
   return result["text"]
 
 # Étape de prétraitement
-def preprocess_text(text):
-  tokens = nltk.word_tokenize(text)
-  tokens = [word.lower() for word in tokens if word.isalpha()]
-  stop_words = set(nltk.stopwords.words('english'))
-  tokens = [word for word in tokens if word not in stop_words]
-  lemmatizer = nltk.WordNetLemmatizer()
-  tokens = [lemmatizer.lemmatize(word) for word in tokens]
-  return tokens
+#def preprocess_text(text):
+#  tokens = nltk.word_tokenize(text)
+#  tokens = [word.lower() for word in tokens if word.isalpha()]
+#  stop_words = set(nltk.stopwords.words('english'))
+#  tokens = [word for word in tokens if word not in stop_words]
+#  lemmatizer = nltk.WordNetLemmatizer()
+#  tokens = [lemmatizer.lemmatize(word) for word in tokens]
+#  return tokens
 
 def convert_text_to_mp3(text, language='en'):
   myobj = gTTS(text=text, lang=language, slow=False)
   myobj.save("retro.mp3")
-  os.system("start retro.mp3")
+  os.system("mpg123 retro.mp3")
 
 def main():
   # Enregistrement de l'audio
-  audioFilename = 'output.wav'
+  audioFilename = 'input.wav'
   time = 5
   frequency = 44100
   record_audio(fs=frequency, seconds=time, audioFilename=audioFilename)
   # Transcription de l'audio
   transcribed_text = transcribe_audio(model, filename=audioFilename)
-  preprocessed_text = preprocess_text(transcribed_text)
-  print(preprocessed_text)
+  #preprocessed_text = preprocess_text(transcribed_text)
+  print(transcribed_text)
+
   # Conversion du texte transcrit en mp3
-  #convert_text_to_mp3(transcribed_text, language='en')
+  convert_text_to_mp3(transcribed_text, language='en')
 
 # Appel de la fonction main
 if __name__ == "__main__":
