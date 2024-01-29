15313e3aa22a28cdfcb2c15bc9c77d44071414d7
tiblond <bera1107@usherbrooke.ca>
2024-01-29 13:29:28 -0500

    Wannabe WS

---
+++ catkin_ws/build/dialogue/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/tiblond/Documents/Homo_DeUS/catkin_ws/src/dialogue
+# Build directory: /home/tiblond/Documents/Homo_DeUS/catkin_ws/build/dialogue
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
